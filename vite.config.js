ed547f6a6ef907827450128b5b0772eb7203dd50
Yvan Blanchette <yvanblanchette@gmail.com>
2026-02-06 19:47:27 -0500

    Fix framer-motion bundling for production build

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,19 @@ export default defineConfig({
 			"@": path.resolve("src"),
 		},
 	},
+	optimizeDeps: {
+		include: ["framer-motion"],
+	},
+	build: {
+		commonjsOptions: {
+			include: [/framer-motion/, /node_modules/],
+		},
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					"framer-motion": ["framer-motion"],
+				},
+			},
+		},
+	},
 });
